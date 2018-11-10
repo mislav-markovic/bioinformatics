6fad3eafec0c9da3b7bce788d49676204b8a4a86
Mislav <mislav.markovic@fer.hr>
2018-11-10 12:48:19 +0100

    Added suffix tree class and node (tree node) class

---
+++ Ukonnen/Ukonnen/suffix_tree.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "suffix_tree.h"
+
+
+suffix_tree::suffix_tree()
+{
+}
+
+
+suffix_tree::~suffix_tree()
+{
+}
---
+++ Ukonnen/Ukonnen/suffix_tree.h
@@ -0,0 +1,11 @@
+#ifndef SUFFIX_TREE_H
+#define SUFFIX_TREE_H
+
+class suffix_tree
+{
+public:
+	suffix_tree();
+	~suffix_tree();
+};
+
+#endif
