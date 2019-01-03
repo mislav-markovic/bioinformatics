15110cd3f46ea97da97e1a0f01339ba2c376f372
Mislav Marković <mislav.markovic@fer.hr>
2019-01-03 02:46:36 +0100

    Comments

--- Ukonnen/Ukonnen/node.h
+++ Ukonnen/Ukonnen/node.h
@@ -1,13 +1,26 @@
 #ifndef NODE_H
 #define NODE_H
 #include <vector>
+#include "suffix_tree.h"
 
 namespace ukonnen
 {
 	class node
 	{
+		//start of part of suffix defined by this node
 		unsigned int from_;
+		//end of suffix denoted by this node.
 		unsigned int to_;
+		//leafs define suffixes that go from some point in text to the end of text.
+		//as text grows to_ member of node does not need to be updated
+		bool is_leaf_;
+		//root node is significant when active node is determined after node splitting occurs
+		bool is_root_;
+		//pointer to this nodes suffix link, nullptr if no link exists.
+		node* suffix_link_;
+		//tree this node is part of, needed for O(1) updates to leaf nodes.
+		suffix_tree const& tree_;
+		//child nodes
 		std::vector<node> children_;
 	};
 }
--- Ukonnen/Ukonnen/suffix_tree.h
+++ Ukonnen/Ukonnen/suffix_tree.h
@@ -5,14 +5,37 @@
 
 namespace ukonnen
 {
+	//active point data container as used here:
+	//https://stackoverflow.com/questions/9452701/ukkonens-suffix-tree-algorithm-in-plain-english
+	struct active_point {
+		//non-owning raw pointer, do not free
+		node* active_node_;
+		//first symbol of a child node of active node.
+		//just first symbol is enough since children are guaranteed to have unique starts of their suffixes.
+		char active_edge_;
+		//how much of child's value is considered.
+		unsigned int active_length_;
+	};
+
   class suffix_tree
   {
+	  //root of tree
 	  node root_;
+	  //text that is used to construct tree
+	  //add method should add to this string
+	  //maybe tree shouldn't own text??
+	  std::string text_;
+	  //node constructed in previous step (needed in some steps of algorithm)
+	  //non-owning raw pointer, do not free
+	  node* prev_node_;
+	  //how many suffixes need to be inserted into tree.
+	  unsigned int remainder_;
   public:
     suffix_tree();
-    ~suffix_tree();
+	//add symbol as suffix to tree and text
     void add(char symbol);
-    [[nodiscard]] bool contains(std::string const&) const noexcept;
+	//check if suffix is contained in tree
+    [[nodiscard]] bool contains(std::string const& suffix) const noexcept;
   };
 }
 #endif //SUFFIX_TREE_H
