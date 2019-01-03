55d9da04079f7a813c88196c7829ab3608b17f0b
Mislav Marković <mislav.markovic@fer.hr>
2019-01-03 15:48:30 +0100

    Interface changes

--- Ukonnen/Ukonnen/suffix_tree.cpp
+++ Ukonnen/Ukonnen/suffix_tree.cpp
@@ -2,10 +2,18 @@
 
 const char NON_ACTIVE_EDGE = '\0x';
 
-suffix_tree::suffix_tree() : root_{}, text_{}, prev_node_{ nullptr }, remainder_{ 0 }, active_point_{}
+suffix_tree::suffix_tree() : root_{}, text_{}, prev_node_{ nullptr }, remainder_{ 0 }, active_point_{root_}
 {
 }
 
-active_point_t::active_point_t() : active_node_{ nullptr }, active_edge_{ NON_ACTIVE_EDGE }, active_length_{ 0 }
+void suffix_tree::add(char symbol)
+{
+	//add symbol that is being processed to the end of text (updating all leaf nodes in process)
+	text_ += symbol;
+
+
+}
+
+active_point_t::active_point_t(node& active_node) : active_node_{ active_node }, active_edge_{ NON_ACTIVE_EDGE }, active_length_{ 0 }
 {
 }
